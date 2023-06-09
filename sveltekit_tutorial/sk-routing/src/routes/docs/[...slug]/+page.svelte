2fa2ca353deb60c13b0803db98a003c164e8c067
Jake1152 <rancho974@gmail.com>
2023-06-09 20:38:16 +0900

    ADD
    catch all routes
    
    - [...slug]
    - e.g)
    	http://localhost:5173/docs/42/13

---
+++ sveltekit_tutorial/sk-routing/src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { page } from '$app/stores';
+    // const { productId, reviewId } = $page.params.slug;
+    console.log($page.params.slug)
+    // 어떤 경로로 해서 들어왔는지 다 알려준다.
+
+    const slugArr = $page.params.slug.split('/');
+    console.log(slugArr);
+</script>
+
+<h1> Docs 페이지 </h1>
+<!-- <h1> 제품{productId}의 리뷰 {reviewId}번</h1> -->
+
+
+{#if slugArr.length === 1}
+<h1>Docs 페이지 ; 주제 {slugArr[0]}</h1>
+{:else if slugArr.length === 2}
+<h1> Docs 페이지 ; 주제 {slugArr[0]}; 소주제 {slugArr[1]} </h1>
+{:else}
+    let h1 = document.createElement('h1')
+    h1.innerText = "Docs 페이지 "
+    for (let idx : number = 1; idx < slugArr.length; idx++) {
+        h1.innerText += "; 소주제 " {slugArr[idx]}
+    }
+{/if}
+<!-- 
+slug갯수만큼 처리할 수 있다.
+ -->
