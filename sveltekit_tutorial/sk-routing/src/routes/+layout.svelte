fd0427158d5ff22b060a3fa226406829094e9c2b
Jake1152 <rancho974@gmail.com>
2023-06-10 22:02:22 +0900

    ADD
    - routes layout
    헤더, 푸터, 그외에 하위 페이지에 공통 적용하고 싶은 사항을 적는다.

---
+++ sveltekit_tutorial/sk-routing/src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<!-- 
+    하위에 공통된 layout을 넣을때 사용한다.
+ -->
+<header class="layout-header"> 헤더 영역</header>
+<slot />
+<footer class="layout-footer">푸터 영역</footer>
+
+<style>
+    .layout-header {
+        background-color: #000000;
+        color: #ffffff;
+        font-size: 30px;
+        text-align: center;
+        padding: 10px;
+    }
+
+    .layout-footer {
+        background-color: #000000;
+        color: #ffffff;
+        font-size: 30px;
+        text-align: center;
+        padding: 10px;
+    }
+</style>
+<!-- 슬롯은 구멍
+값이 대입됨 
+다른 dir밑에 있는 page.svelte에 있는 content가 <slot/> 부분에 삽입된다.Layout
+그러므로 slot이라는 테그는 항상 필요하다.
+-->
---
+++ sveltekit_tutorial/sk-routing/src/routes/products/[productid]/+layout.svelte
@@ -0,0 +1,7 @@
+<slot />
+
+<hr />
+<h3> 제품 배송 반품 관련 설명</h3>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, doloremque repudiandae quas id iusto nulla deserunt sed voluptate autem aliquid, eum non officia. Veritatis perferendis provident porro quasi minus non!
+</p>
